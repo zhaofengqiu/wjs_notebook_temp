73b56dc6572c0bf9c177a60a5a248d944a65fedb
zhaofengqiu <1694851090@qq.com>
2019-06-16 19:23:17 +0800

    "2019-06-16-19:23:17"

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-date2="\"$(date +%Y-%m-%d)' '$(date +%H:%M:%S)"\"
+date2="\"$(date +%Y-%m-%d)-$(date +%H:%M:%S)"\"
 git add .
+echo $date2
+git commit -m $date2
 
-git commit -m $date2 $1&>/dev/null
-
-git push -u origin master $1&>/dev/null
+git push -u origin master
